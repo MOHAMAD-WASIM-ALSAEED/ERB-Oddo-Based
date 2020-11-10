f7e81d5aa59c9fc7dc3a7db5b2415353df77874d
CMMI_Admin <CMMI_Admin@MEDU-CMMI>
2020-11-10 17:24:21 +0300

    Create Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using oddo.Models;
 using oddo.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.VisualBasic;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace oddo.Controllers
 {
@@ -166,6 +167,112 @@ namespace oddo.Controllers
             var Partner = _hRContext.ResPartner.FirstOrDefault(x=>x.Id==id)??new ResPartner();
             var Data = new SecondaryDetailViewModel { User = Partner, BreadCrumbsEmployees = EmployeeBreadCrumbs };
             return View(Data);
+        }
+        [HttpGet]
+        public IActionResult CreateEmployee(int id= 30) {
+
+            List<Employee> employeesTree = new List<Employee>();
+            List<Employee> EmployeeBreadCrumbs = new List<Employee>();
+            List<Employee> EmployeeWithSameManeger = new List<Employee>();
+            string BreadCrumbsIds = "";
+            var Employee = _hRContext.Employee.FirstOrDefault(s => s.Id == id) ?? new Employee();
+            BreadCrumbsIds = HttpContext.Session.GetString("Employee");
+            BreadCrumbsIds += "-" + id.ToString();
+            HttpContext.Session.SetString("Employee", BreadCrumbsIds);
+            if (BreadCrumbsIds.Split("-").Length > 1)
+            {
+                foreach (var item in BreadCrumbsIds.Split("-"))
+                {
+                    if (EmployeeBreadCrumbs.Where(x => x.Id == Convert.ToDouble(item)).Count() > 0)
+                    {
+                        var index = EmployeeBreadCrumbs.IndexOf(EmployeeBreadCrumbs.Where(x => x.Id == Convert.ToDouble(item)).FirstOrDefault());
+                        EmployeeBreadCrumbs.RemoveRange(index + 1, EmployeeBreadCrumbs.Count - index - 1);
+                        continue;
+
+                    }
+                    if (!(item is null) && item != "")
+                    {
+                        EmployeeBreadCrumbs.Add(_hRContext.Employee.Where(x => x.Id.ToString() == item).FirstOrDefault());
+                    }
+                }
+            }
+            var Tags = _hRContext.Tags.Where(x => x.EmpId == Employee.Id).Select(x => x.CategoryId).ToList<double?>();
+            var TagVAlues = _hRContext.TagValue.Where(x => Tags.Contains(x.Id)).ToList<TagValue>();
+            var department = _hRContext.Department.Where(x => x.Id == Employee.DepartmentId).FirstOrDefault();
+            var EmployeeManeger = _hRContext.Employee.FirstOrDefault(m => m.Id == Employee.ParentId) ?? new Employee();
+            var EmployeeCoach = _hRContext.Employee.FirstOrDefault(m => m.Id == Employee.CoachId) ?? new Employee();
+            var EmployeepartnerId = _hRContext.User.FirstOrDefault(m => m.Id == Employee.LeaveManagerId.ToString()) ?? new User();
+            var timeoff = _hRContext.ResPartner.FirstOrDefault(m => m.Id.ToString() == EmployeepartnerId.PartnerId) ?? new ResPartner();
+            var EmployeepartnerRelatedId = _hRContext.User.FirstOrDefault(m => m.Id == Employee.UserId.ToString()) ?? new User();
+            var RelatedUser = _hRContext.ResPartner.FirstOrDefault(m => m.Id.ToString() == EmployeepartnerRelatedId.PartnerId) ?? new ResPartner();
+            var Country = _hRContext.Country.FirstOrDefault(m => m.Id == Employee.CountryId);
+            var employeeLoop = _hRContext.Employee.FirstOrDefault(s => s.Id == id);
+            var dependant = _hRContext.Dependent.Where(x => x.EmployeeDependantId == id).ToList<Dependent>();
+            var Jobs = _hRContext.Jobs.FirstOrDefault(x => x.Id == Employee.JobId) ?? new Jobs();
+            var resourcesCalender = _hRContext.ResourceCalendar.FirstOrDefault(x => x.Id == Employee.ResourceCalendarId) ?? new ResourceCalendar();
+            var resources = _hRContext.Resources.FirstOrDefault(x => x.Id == Employee.ResourceId) ?? new Resources();
+            var image = _hRContext.Image.FirstOrDefault(x => x.EmployeeId == Employee.Id) ?? new image();
+
+            foreach (var item in dependant)
+            {
+                var birthday = item.Bdate;
+                var daycount = ((DateAndTime.Now - birthday.Value).TotalDays);
+                if (daycount < 730)
+                {
+                    item.Type = "Baby";
+                }
+                else if (daycount >= 730 && daycount < 4380)
+                {
+                    item.Type = "Child";
+                }
+                else
+                {
+                    item.Type = "Adult";
+                }
+            }
+
+            while (employeeLoop.ParentId != null)
+            {
+                var parentid = employeeLoop.ParentId;
+                employeeLoop = _hRContext.Employee.FirstOrDefault(s => s.Id == parentid);
+                employeesTree.Add(employeeLoop);
+            }
+            foreach (var item in _hRContext.Employee.ToList<Employee>())
+            {
+                if (item.ParentId == id) EmployeeWithSameManeger.Add(item);
+            }
+            employeesTree.Reverse();
+            employeesTree.Add(Employee);
+            foreach (var item in employeesTree)
+            {
+                item.Job = _hRContext.Jobs.FirstOrDefault(x => x.Id == item.JobId) ?? new Jobs();
+            }
+
+            foreach (var item in EmployeeWithSameManeger)
+            {
+                item.Job = _hRContext.Jobs.FirstOrDefault(x => x.Id == item.JobId) ?? new Jobs();
+            }
+            EmployeeViewModel employeeViewModel = new EmployeeViewModel
+            {
+                Employee = Employee,
+                Tags = TagVAlues,
+                Department = department,
+                Maneger = EmployeeManeger,
+                Coach = EmployeeCoach,
+                TimeOff = timeoff,
+                RelatedUser = RelatedUser,
+                CountryName = Country.Name,
+                EmployeeTree = employeesTree,
+                BreadCrumbsEmployees = EmployeeBreadCrumbs,
+                EmployeeDependents = dependant,
+                EmployeeWithSameManeger = EmployeeWithSameManeger,
+                Job = Jobs,
+                Timezone = resources,
+                ResourceCalendar
+            = resourcesCalender,
+                employeeImage = "data:image/png;base64," + image.ImageCode
+            };
+            return View(employeeViewModel);
         }
     }
 }
